e7f2b530d811592ee84361c27c388044d84bcb7f
Coby Yates <cobylyates@gmail.com>
2020-03-25 15:17:13 -0600

    inital commit

--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,135 @@
 <template>
-  <v-app>
+  <v-app id="inspire">
+    <v-navigation-drawer
+      v-model="drawer"
+      app
+      clipped
+    >
+      <v-list dense>
+        <v-list-item
+          v-for="item in items"
+          :key="item.text"
+          link
+        >
+          <v-list-item-action>
+            <v-icon>{{ item.icon }}</v-icon>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>
+              {{ item.text }}
+            </v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+        <!-- <v-subheader class="mt-4 grey--text text--darken-1">SUBSCRIPTIONS</v-subheader> -->
+        <!-- <v-list>
+          <v-list-item
+            v-for="item in items2"
+            :key="item.text"
+            link
+          >
+            <v-list-item-avatar>
+              <img
+                :src="`https://randomuser.me/api/portraits/men/${item.picture}.jpg`"
+                alt=""
+              >
+            </v-list-item-avatar>
+            <v-list-item-title v-text="item.text" />
+          </v-list-item>
+        </v-list> -->
+        <!-- <v-list-item
+          class="mt-4"
+          link
+        >
+          <v-list-item-action>
+            <v-icon color="grey darken-1">mdi-plus-circle-outline</v-icon>
+          </v-list-item-action>
+          <v-list-item-title class="grey--text text--darken-1">Browse Channels</v-list-item-title>
+        </v-list-item>
+        <v-list-item link>
+          <v-list-item-action>
+            <v-icon color="grey darken-1">mdi-settings</v-icon>
+          </v-list-item-action>
+          <v-list-item-title class="grey--text text--darken-1">Manage Subscriptions</v-list-item-title>
+        </v-list-item> -->
+      </v-list>
+    </v-navigation-drawer>
+
     <v-app-bar
       app
-      color="primary"
-      dark
+      clipped-left
+      color="light-blue darken-3"
+      dense
     >
-      <div class="d-flex align-center">
-        <v-img
-          alt="Vuetify Logo"
-          class="shrink mr-2"
-          contain
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
-          transition="scale-transition"
-          width="40"
-        />
-
-        <v-img
-          alt="Vuetify Name"
-          class="shrink mt-1 hidden-sm-and-down"
-          contain
-          min-width="100"
-          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
-          width="100"
-        />
-      </div>
-
-      <v-spacer></v-spacer>
-
-      <v-btn
-        href="https://github.com/vuetifyjs/vuetify/releases/latest"
-        target="_blank"
-        text
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer" />
+      <v-icon
+        class="mx-4"
+        large
       >
-        <span class="mr-2">Latest Release</span>
-        <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+        mdi-image-filter-hdr
+      </v-icon>
+      <v-toolbar-title class="mr-12 align-center">
+        <span class="headline white--text">LONE<span class="green--text">PEAK</span> <span class="body-2">Visualization Group</span></span>
+      </v-toolbar-title>
+      <v-spacer />
+      <v-row
+        align="center"
+        style="max-width: 650px"
+      >
+        <v-text-field
+          :append-icon-cb="() => {}"
+          placeholder="Search..."
+          single-line
+          append-icon="mdi-magnify"
+          color="white"
+          hide-details
+        />
+      </v-row>
     </v-app-bar>
 
     <v-content>
-      <HelloWorld/>
+      <v-container class="fill-height" fluid>
+        <Home />
+      </v-container>
     </v-content>
   </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld';
+import Home from './components/Home'
 
-export default {
-  name: 'App',
-
-  components: {
-    HelloWorld,
-  },
-
-  data: () => ({
-    //
-  }),
-};
+  export default {
+    props: {
+      source: String,
+    },
+    data: () => ({
+      drawer: null,
+      items: [
+        { icon: 'mdi-plus', text: 'Add New Case' },
+        { icon: 'mdi-check', text: 'Task List' },
+        { icon: 'mdi-account-multiple-check', text: 'Manager Task List' },
+        { icon: 'mdi-account-check', text: 'Expert Task List' },
+        { icon: 'mdi-domain', text: 'Associate Management' },
+        { icon: 'mdi-account-group', text: 'Clients & Contacts' },
+        { icon: 'mdi-trending-up', text: 'Marketing' },
+        { icon: 'mdi-text-box-plus', text: 'New Cases' },
+        { icon: 'mdi-text-box-check', text: 'Cases Closing' },
+        { icon: 'mdi-text-box-remove', text: 'Closed Cases' },
+        { icon: 'mdi-bullseye-arrow', text: 'Template Accuracy' },
+        { icon: 'mdi-cog', text: 'Settings' },
+      ],
+      items2: [
+        { picture: 28, text: 'Joseph' },
+        { picture: 38, text: 'Apple' },
+        { picture: 48, text: 'Xbox Ahoy' },
+        { picture: 58, text: 'Nokia' },
+        { picture: 78, text: 'MKBHD' },
+      ],
+    }),
+    components: {
+      Home
+    },
+    created () {
+      this.$vuetify.theme.dark = true
+    },
+  }
 </script>
---
+++ src/components/Home.vue
@@ -0,0 +1,247 @@
+<template>
+  <v-container fluid class="fluid">
+    <v-row>
+      <!-- <v-col cols="1" class="mx-3">
+        <p>Projects</p>
+        <v-row v-for="project in projects" :key="project.i">
+          <v-btn width="150" class="my-1">{{ project.name }}</v-btn>
+        </v-row>
+      </v-col> -->
+      <v-col cols="1" class="mx-3">
+        <p>Staff</p>
+        <v-row v-for="staff in staffs" :key="staff.i">
+          <v-btn width="150" class="my-1">{{ staff.name }}</v-btn>
+        </v-row>
+      </v-col>
+      <v-col cols="7" class="mx-3">
+        <p>Clients</p>
+        <v-data-table
+          dense
+          :headers="headers"
+          :items="clients"
+          item-key="name"
+          class="elevation-1"
+        ></v-data-table>
+      </v-col>
+      <v-col cols="3" class="mx-3">
+        <p>Tasks</p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+
+  data: () => ({
+    // projects: [
+    //   { name: "Project 1" },
+    //   { name: "Project 2" },
+    //   { name: "Project 3" },
+    //   { name: "Project 4" },
+    //   { name: "Project 5" },
+    //   { name: "Project 6" },
+    //   { name: "Project 7" },
+    //   { name: "Project 8" },
+    //   { name: "Project 9" },
+    //   { name: "Project 10" },
+    //   { name: "Project 11" },
+    //   { name: "Project 12" },
+    //   { name: "Project 13" },
+    //   { name: "Project 14" },
+    //   { name: "Project 15" },
+    //   { name: "Project 16" },
+    //   { name: "Project 17" }
+    // ],
+    staffs: [
+      { name: "Shawn" },
+      { name: "Jane" },
+      { name: "Ronald" },
+      { name: "Joe" },
+      { name: "Dylan" },
+      { name: "Joshua" },
+      { name: "Ralph" },
+      { name: "Cory" },
+      { name: "Jordan" },
+      { name: "Jack" },
+      { name: "Carl" }
+    ],
+    clients: [
+      {
+        name: "Frozen Yogurt",
+        contactName: 159,
+        phone: 6.0,
+        nextContact: 24,
+        caseManager: 4.0,
+        caseExpert: "1%"
+      },
+      {
+        name: "Ice cream sandwich",
+        contactName: 237,
+        phone: 9.0,
+        nextContact: 37,
+        caseManager: 4.3,
+        caseExpert: "1%"
+      },
+      {
+        name: "Eclair",
+        contactName: 262,
+        phone: 16.0,
+        nextContact: 23,
+        caseManager: 6.0,
+        caseExpert: "7%"
+      },
+      {
+        name: "Cupcake",
+        contactName: 305,
+        phone: 3.7,
+        nextContact: 67,
+        caseManager: 4.3,
+        caseExpert: "8%"
+      },
+      {
+        name: "Gingerbread",
+        contactName: 356,
+        phone: 16.0,
+        nextContact: 49,
+        caseManager: 3.9,
+        caseExpert: "16%"
+      },
+      {
+        name: "Jelly bean",
+        contactName: 375,
+        phone: 0.0,
+        nextContact: 94,
+        caseManager: 0.0,
+        caseExpert: "0%"
+      },
+      {
+        name: "Lollipop",
+        contactName: 392,
+        phone: 0.2,
+        nextContact: 98,
+        caseManager: 0,
+        caseExpert: "2%"
+      },
+      {
+        name: "Honeycomb",
+        contactName: 408,
+        phone: 3.2,
+        nextContact: 87,
+        caseManager: 6.5,
+        caseExpert: "45%"
+      },
+      {
+        name: "Donut",
+        contactName: 452,
+        phone: 25.0,
+        nextContact: 51,
+        caseManager: 4.9,
+        caseExpert: "22%"
+      },
+      {
+        name: "KitKat",
+        contactName: 518,
+        phone: 26.0,
+        nextContact: 65,
+        caseManager: 7,
+        caseExpert: "6%"
+      },
+      {
+        name: "Frozen Yogurt",
+        contactName: 159,
+        phone: 6.0,
+        nextContact: 24,
+        caseManager: 4.0,
+        caseExpert: "1%"
+      },
+      {
+        name: "Ice cream sandwich",
+        contactName: 237,
+        phone: 9.0,
+        nextContact: 37,
+        caseManager: 4.3,
+        caseExpert: "1%"
+      },
+      {
+        name: "Eclair",
+        contactName: 262,
+        phone: 16.0,
+        nextContact: 23,
+        caseManager: 6.0,
+        caseExpert: "7%"
+      },
+      {
+        name: "Cupcake",
+        contactName: 305,
+        phone: 3.7,
+        nextContact: 67,
+        caseManager: 4.3,
+        caseExpert: "8%"
+      },
+      {
+        name: "Gingerbread",
+        contactName: 356,
+        phone: 16.0,
+        nextContact: 49,
+        caseManager: 3.9,
+        caseExpert: "16%"
+      },
+      {
+        name: "Jelly bean",
+        contactName: 375,
+        phone: 0.0,
+        nextContact: 94,
+        caseManager: 0.0,
+        caseExpert: "0%"
+      },
+      {
+        name: "Lollipop",
+        contactName: 392,
+        phone: 0.2,
+        nextContact: 98,
+        caseManager: 0,
+        caseExpert: "2%"
+      },
+      {
+        name: "Honeycomb",
+        contactName: 408,
+        phone: 3.2,
+        nextContact: 87,
+        caseManager: 6.5,
+        caseExpert: "45%"
+      },
+      {
+        name: "Donut",
+        contactName: 452,
+        phone: 25.0,
+        nextContact: 51,
+        caseManager: 4.9,
+        caseExpert: "22%"
+      },
+      {
+        name: "KitKat",
+        contactName: 518,
+        phone: 26.0,
+        nextContact: 65,
+        caseManager: 7,
+        caseExpert: "6%"
+      }
+    ],
+    headers: [
+      {
+        text: "Case Name",
+        align: "start",
+        sortable: false,
+        value: "name"
+      },
+      { text: "Contact Name", value: "contactName" },
+      { text: "Phone", value: "phone" },
+      { text: "Next Contact", value: "nextContact" },
+      { text: "Case Manager", value: "caseManager" },
+      { text: "Case Expert", value: "caseExpert" }
+    ]
+  })
+};
+</script>
